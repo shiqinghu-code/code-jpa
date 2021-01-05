42f0d367124169cddf16b6856e9749c09cc4f6d9
shiqinghu <1036053871@qq.com>
2021-01-05 23:22:10 +0800

    生成代码以及参数校验

---
+++ src/main/java/com/qing/hu/CodeJpaApp.java
@@ -0,0 +1,13 @@
+package com.qing.hu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeJpaApp 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(CodeJpaApp.class, args);
+    }
+}
